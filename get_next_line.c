7481a52a4afde5a0eca2ad1b11344ff5acc6d41f
Polpi <anthotuigr@gmail.com>
2022-11-17 19:24:05 +0100

    pas trop avancé

--- get_next_line.c
+++ get_next_line.c
@@ -6,17 +6,27 @@
 /*   By: polpi <polpi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 15:42:39 by polpi             #+#    #+#             */
-/*   Updated: 2022/11/17 16:15:12 by polpi            ###   ########.fr       */
+/*   Updated: 2022/11/17 19:23:52 by polpi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+// Tout ce passe avec read ! 
+
+/*
 char	*get_next_line(int fd)
 {
+	char	*buffer;
+	char	*stash;
+	int		i;
 
+	stash = NULL;
+	buffer = malloc
+
+	return ("few");
 }
-
+*/
 int	main (int argc, char **argv)
 {
 	(void)argc;
@@ -24,6 +34,6 @@ int	main (int argc, char **argv)
 	int	fd;
 
 	fd = open("text.txt", O_RDONLY);
-	//printf("%d", fd);
-	get_next_line(fd);
+	printf("%d", fd);
+	//get_next_line(fd);
 }
