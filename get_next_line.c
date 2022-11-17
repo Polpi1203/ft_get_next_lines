193f9233eaa4c0b59d5fc12505f976e8f6490ff2
Polpi <anthotuigr@gmail.com>
2022-11-17 16:15:26 +0100

    begin

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,16 @@
 /*   By: polpi <polpi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 15:42:39 by polpi             #+#    #+#             */
-/*   Updated: 2022/11/17 16:06:14 by polpi            ###   ########.fr       */
+/*   Updated: 2022/11/17 16:15:12 by polpi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-//char	*get_next_line(int fd)
+char	*get_next_line(int fd)
+{
+
+}
 
 int	main (int argc, char **argv)
 {
@@ -21,5 +24,6 @@ int	main (int argc, char **argv)
 	int	fd;
 
 	fd = open("text.txt", O_RDONLY);
-	printf("%d", fd);
+	//printf("%d", fd);
+	get_next_line(fd);
 }
